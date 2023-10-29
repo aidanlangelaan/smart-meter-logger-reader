f7da1d7e12c09700d0656c32120f1b8f6225783b
Aidan Langelaan <aidanlangelaan@gmail.com>
2023-10-29 14:39:23 +0100

    added debug endpoint and environment config file

---
+++ config.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# Config for reading and parsing smart-meter telegrams
+# 2023-10 v1.0
+app_settings = dict(
+    environment='development',
+    api_url='http://localhost:5000/api/',
+)
